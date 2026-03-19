9549e3c7bf121895b0aee818a5450a2d66d3a2cb
Vinod Bhavnani <vinodbhavnani@Vinods-MacBook-Pro.local>
2026-03-19 15:58:45 +0700

    chore: gh-pages changes

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react()],
+  base: "/facebook-chat/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
